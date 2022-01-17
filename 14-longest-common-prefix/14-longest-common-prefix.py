3a176cdd02e56802945985face2ab1ce7117dac7
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2022-01-16 21:29:29 -0600

    Time: 24 ms (98.83%), Space: 14.3 MB (56.47%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -8,8 +8,8 @@ class Solution:
         for i in range(len(shortest_str)):
             for s in strs:
                 if shortest_str[i] != s[i]:
-                    print (s[i])
                     return prefix
-            prefix = shortest_str[:i+1]
+            prefix += shortest_str[i]
+            #print(shortest_str[:i+1])
         
         return prefix
