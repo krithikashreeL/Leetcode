90681bbf9c641609f17291b78f2e02c6d01f02e2
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2022-01-18 18:09:34 -0600

    Time: 369 ms (17.83%), Space: 14.5 MB (11.12%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        i2 = i3 = i5 = 0
+        ugly_list = [1]
+        while len(ugly_list) < n:
+            nextnum = min(2*ugly_list[i2],3*ugly_list[i3],5*ugly_list[i5])
+            if nextnum == 2*ugly_list[i2] : i2+=1
+            if nextnum == 3*ugly_list[i3] : i3+=1
+            if nextnum == 5*ugly_list[i5] : i5+=1
+            ugly_list.append(nextnum)
+            print(nextnum)
+        return ugly_list[-1]
+            
+            
