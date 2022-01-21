c2be8fa6db6ea7532a5e842d56da714deac6d538
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2022-01-21 01:33:38 -0600

    Time: 166 ms (47.30%), Space: 15.1 MB (43.94%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,27 +1,13 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        # rows = len(matrix) 
-        # cols = len(matrix[0])
-        # for i in range (1,rows):
-        #     for j in range (cols):
-        #         if j == 0:
-        #             matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j+1])
-        #         if j == cols-1:
-        #             matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j-1])
-        #         else:
-        #             matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j-1],matrix[i-1][j+1])
-        # return min(matrix[rows-1])
-        
-        r = len(matrix)
-        c = len(matrix[0])
-        
-        for i in range(1, r):
-            for j in range(c):
+        rows = len(matrix) 
+        cols = len(matrix[0])
+        for i in range (1,rows):
+            for j in range (cols):
                 if j == 0:
-                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1])
-                elif j == c-1:
-                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1])
+                    matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j+1])
+                elif j == cols-1:
+                    matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j-1])
                 else:
-                    matrix[i][j] += min(min(matrix[i-1][j], matrix[i-1][j-1]), matrix[i-1][j+1])
-        
-        return min(matrix[r-1])
+                    matrix[i][j] += min(matrix[i-1][j],matrix[i-1][j-1],matrix[i-1][j+1])
+        return min(matrix[rows-1])
