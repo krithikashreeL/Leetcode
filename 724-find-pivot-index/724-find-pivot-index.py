823ba364993d66a469df163564a1e046c4ecaa0f
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2022-08-25 13:16:31 -0500

    Time: 217 ms (64.32%), Space: 15.3 MB (48.84%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,17 +1,11 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        
-        total_sum = sum(nums)
-        seen_sum = 0
-        
-        # concept here is subtract sum upto that point to total see if rreminder = seen sum meaning left sum = right sum so thats our index
-        
-        
+        total = sum(nums)
+        seen = 0
         
         for i in range(len(nums)):
-            if total_sum - seen_sum - nums[i] == seen_sum:
+            if total - seen - nums[i] == seen:
                 return i
-            seen_sum += nums[i]
-        
+            seen += nums[i]
+            
         return -1
-        
